753d9de15cd02389305f421617293549f472b0a1
Didix  Whyy <didikwahyu1121@gmail.com>
2024-08-02 16:52:20 +0000

    update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,3 +1,3 @@
-const deployedAddress = '0x042bec98b62957054f5Cd764D9BeB6842dF0d595'
+const deployedAddressdeployedAddress = '0x042bec98b62957054f5Cd764D9BeB6842dF0d595'
 
 export default deployedAddress
